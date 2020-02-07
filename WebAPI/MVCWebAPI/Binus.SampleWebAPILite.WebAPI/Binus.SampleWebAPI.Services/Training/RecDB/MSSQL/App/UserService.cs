caf4035946bbe0ab8a758f0a10c751da8c2390b9
Corushoo <49095851+Corushoo@users.noreply.github.com>
2020-02-07 20:07:42 +0700

    add token to reset password

--- WebAPI/MVCWebAPI/Binus.SampleWebAPILite.WebAPI/Binus.SampleWebAPI.Services/Training/RecDB/MSSQL/App/UserService.cs
+++ WebAPI/MVCWebAPI/Binus.SampleWebAPILite.WebAPI/Binus.SampleWebAPI.Services/Training/RecDB/MSSQL/App/UserService.cs
@@ -18,6 +18,11 @@ namespace Binus.SampleWebAPI.Services.Training.RecDB.MSSQL.App
         Task<ExecuteResult> RegisterUser(UserModel User);
         Task<List<UserModel>> GetUnapprovedUser();
         Task<ExecuteResult> VerifyUser(UserModel user);
+        Task<ExecuteResult> GenerateUserToken(UserModel User);
+        Task<String> GetUserToken(UserModel user);
+        String RandomStringBuilder();
+        Task<ExecuteResult> DeleteUserToken(UserModel User);
+
     }
 
     public class UserService : IUserService
@@ -94,9 +99,9 @@ namespace Binus.SampleWebAPI.Services.Training.RecDB.MSSQL.App
         public async Task<ExecuteResult> VerifyUser(UserModel User)
         {
             var Param = new SqlParameter[]
-        {
+            {
                 new SqlParameter("@Email", User.Email)
-        };
+            };
 
             List<StoredProcedure> Data = new List<StoredProcedure>();
             Data.Add(new StoredProcedure {
@@ -107,6 +112,66 @@ namespace Binus.SampleWebAPI.Services.Training.RecDB.MSSQL.App
             ExecuteResult Result = (await _UserRepo.ExecMultipleSPWithTransactionAsync(Data));
 
             return Result;
+        }
+
+        public String RandomStringBuilder()
+        {
+            StringBuilder builder = new StringBuilder();
+            Random random = new Random();
+            char ch;
+            for (int i = 0; i < 30; i++) {
+                ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
+                builder.Append(ch);
+            };
+            return builder.ToString();
+        }
+
+        public async Task<ExecuteResult> GenerateUserToken(UserModel User)
+        {
+            var Param = new SqlParameter[]
+            {
+                new SqlParameter("@Email", User.Email),
+                new SqlParameter("@Token", RandomStringBuilder())
+            };
+
+            List<StoredProcedure> Data = new List<StoredProcedure>();
+            Data.Add(new StoredProcedure {
+                SPName = "bn_RecDB_InsertUserToken @Email, @Token",
+                SQLParam = Param
+            });
+
+            ExecuteResult Result = (await _UserRepo.ExecMultipleSPWithTransactionAsync(Data));
+
+            return Result;
+        }
+
+        public async Task<ExecuteResult> DeleteUserToken(UserModel User)
+        {
+            var Param = new SqlParameter[]
+            {
+                new SqlParameter("@Email", User.Email)
+            };
+
+            List<StoredProcedure> Data = new List<StoredProcedure>();
+            Data.Add(new StoredProcedure {
+                SPName = "bn_RecDB_DeleteUserToken @Email",
+                SQLParam = Param
+            });
+
+            ExecuteResult Result = (await _UserRepo.ExecMultipleSPWithTransactionAsync(Data));
+
+            return Result;
+        }
+
+
+        public async Task<String> GetUserToken(UserModel user)
+        {
+            UserModel User = (await _UserRepo.ExecSPToSingleAsync("bn_RecDB_GetUserToken @Email",
+               new SqlParameter[]
+               {
+                    new SqlParameter("@Email", user.Email),
+               }));
+            return User.Token;
         }
     }
 }
